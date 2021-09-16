5da747922c6607c4a8381b3966e0a77635782451
canadianjennifer <canadianjennifer@gmail.com>
2021-09-16 09:07:09 -0400

    update

---
+++ story_content/user.js
@@ -0,0 +1,118 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6JBafn1gvIE":
+        Script1();
+        break;
+      case "5Wwoh3Ijw09":
+        Script2();
+        break;
+      case "6jnrr9NxAqh":
+        Script3();
+        break;
+      case "620dd0TTeRg":
+        Script4();
+        break;
+      case "6KWSubB2YYX":
+        Script5();
+        break;
+      case "5y57qxN86cT":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.pause();
+audio.currentTime = 0;
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+
+function add_line() {
+	var line = document.createElement("audio");
+	var head=document.getElementsByTagName('body')[0];
+	line.type = "audio/mp3";
+	line.src="";
+	line.id="bgSong" ;
+	line.autoplay = true;
+	line.loop = false;
+	head.appendChild(line);
+}
+
+//We only want to add these once!
+if(document.getElementById('bgSong')==null){
+	add_line();
+	var audio = document.getElementById('bgSong');
+	audio.volume = 0.5;
+	player.SetVar("javascriptsLoaded",true)
+}
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+this.audioFile = player.GetVar("audioFile");
+var audio = document.getElementById('bgSong');
+audio.src=Location + audioFile;
+audio.load();
+audio.play();
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+var LearnerName = player.GetVar('LearnerName');
+var CorrectAnswer = player.GetVar('CorrectAnswer');
+
+var formData = new FormData();
+formData.append('LearnerName', LearnerName);
+formData.append('CorrectAnswer', CorrectAnswer);
+
+
+var request = new XMLHttpRequest();
+request.open('POST', 'https://script.google.com/macros/s/AKfycbwH9MKCIq73QMCT3eIVeX8T8wGIbAl5XpNV9gRw9J89bwwMuzdfShV2HXUV0jXaRdQ7TA/exec');
+request.send(formData);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+var LearnerName = player.GetVar('LearnerName');
+var CorrectAnswer = player.GetVar('CorrectAnswer');
+
+var formData = new FormData();
+formData.append('LearnerName', LearnerName);
+formData.append('CorrectAnswer', CorrectAnswer);
+
+
+var request = new XMLHttpRequest();
+request.open('POST', 'https://script.google.com/macros/s/AKfycbwH9MKCIq73QMCT3eIVeX8T8wGIbAl5XpNV9gRw9J89bwwMuzdfShV2HXUV0jXaRdQ7TA/exec');
+request.send(formData);
+}
+
